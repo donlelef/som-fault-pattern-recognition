3776e36fa1b70aac9eb4c84f2c3c10e8fc9d92e7
abovethecloud <claudio.peroni93@gmail.com>
2015-03-25 18:45:23 +0100

    Done nothing but a comment change (always mostly as an SVN test)

--- R/NormalPlotTest.R
+++ R/NormalPlotTest.R
@@ -4,5 +4,5 @@ x <- seq(-4, 4, length=100);
 # Normal distribution
 distribution <- dnorm(x);
 
-# Linear plot of the distribution
+# Linear plot (the type specifies the continuity of the line) of the distribution
 plot(distribution, type = 'l');
