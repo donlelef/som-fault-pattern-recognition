6f13da954358823778a029d934eef43b560c2981
Don Lele <donlelef@gmail.com>
2015-03-27 22:49:39 +0100

    package created: KDEBenchmark (see description) file modified: faultMapPlot (functions moved to package)

---
+++ R/KDEBenchmark/R/fillRectangularMap.R
@@ -0,0 +1,25 @@
+#' Fills a grid whith two values according to the probabilityFuncion 
+#' The higher is the value probabilityFunction takes in a cetrain point,the
+#' higher is the probability that pixel is set to faultValue. The probabilityFuncion
+#' is rescaled so that its maximum value is equal to maxFaultProbability.
+#' @param probabilityFunction: a sqare matrix the probability distribution of faults
+#' @param maxFaultProbability: a real positive number which represents the maximum
+#' probability of a fault
+#' @param faultVaue: the value assigned to a element which is faulty. Default is TRUE
+#' @param notFaultVaue: the value assigned to a element which is faulty. Default is FALSE.
+#' @return a matrix of the same dimensions of probabilityFunction.
+
+fillRectangularMap = function(probabilityFunction, maxFaultProbability, faultValue = TRUE, notFaultValue = FALSE){
+  faultMap = matrix(data = 0, nrow = nrow(probabilityFunction), ncol = ncol(probabilityFunction))
+  for (i in 1:nrow(faultMap)){
+    for (j in 1:ncol(faultMap)){
+      if (runif(n = 1, min = 0, max = 1) < (probabilityFunction[i,j]/max(probabilityFunction))*maximumFaultProbability){
+        faultMap[i,j] = TRUE 
+      }
+      else{
+        faultMap[i,j] = FALSE
+      } 
+    }
+  }
+  return(faultMap)
+}
--- R/faultMapPlot.R
+++ R/faultMapPlot.R
@@ -2,12 +2,13 @@
 # This probability function is assumed to represent the probability of a fault
 # to happen on the chip in the coordinates (x1, x2).
 # After that, a map is created where random faults are simulated. The value
-# 0 in the map means 'no fault' and 1 means 'fault', whereas -1 indicates 
-# the points out of the circular wafer.
+# FALSE in the map means 'no fault' and TRUE means 'fault', whereas NA indicates 
+# a point out of the circular wafer.
 
 # Import required libraries
 library(mvtnorm)  # Needed for dmvnorm()
 library(plot3D)   # Needed for mesh()
+library(KDEBenchmark) 
 
 # Initial parameters
 ray = 50
@@ -55,36 +56,3 @@ image2D(
   grid(nx=nrow(faultMap)), ny = ncol(faultMap),
   colkey = FALSE, NAcol = "white",  col = heat.colors(2)
 ) # Colkey = FALSE: no color key legend will be added
-
-
-########################### FUNCTIONS ######################################
-
-# Creates a circular grid from a square one by inserting the value NA in
-# every pixel which is not whitin the circle of ray "ray".
-bindCircularMap = function(rectangularMap, ray) {
-  for (i in 1:nrow(rectangularMap)){
-    for (j in 1:ncol(rectangularMap)){
-      if ((i-ray)^2 + (j-ray)^2 >= ray^2)
-        rectangularMap[i,j]=NA;
-    }
-  }
-  return(rectangularMap) 
-}
-
-# Fill a square grid with FALSE or TRUE according to the probabilityFuncion 
-# The higher is the value probabilityFunction takes in a cetrain point,the
-# higher is the probability that pixel is set to TRUE.
-fillRectangularMap = function(probabilityFunction, maxFaultProbability){
-  faultMap = matrix(data = 0, nrow = nrow(probabilityFunction), ncol = ncol(probabilityFunction))
-  for (i in 1:nrow(faultMap)){
-    for (j in 1:ncol(faultMap)){
-      if (runif(n = 1, min = 0, max = 1) < (probabilityFunction[i,j]/max(probabilityFunction))*maximumFaultProbability){
-        faultMap[i,j] = TRUE 
-      }
-      else{
-        faultMap[i,j] = FALSE
-      } 
-    }
-  }
-  return(faultMap)
-}
