62695de6739239095121d833e3dced6adb9bc11b
Don Lele <donlelef@gmail.com>
2015-03-27 10:27:24 +0100

    Modified files: faultMapPlot (functions inserted)

--- R/faultMapPlot.R
+++ R/faultMapPlot.R
@@ -38,41 +38,53 @@ surf3D(x = X, y = Y, z = Z,
        xlim = c(min(x1),max(x1)), ylim = c(min(x2), max(x2)),
        lighting = TRUE, phi = 30, theta = 45, bty = "b2",
        main = "Bivariate Normal Distribution", sub = bquote(bold(mu[1])==.(mu[1])~
-       ", "~sigma[1]==.(sigma[1,1])~", "~mu[2]==.(mu[2])~", "~sigma[2]==.(sigma[2,2])~
-       ", "~sigma[xy]==.(sigma[2,1])))
+                                                              ", "~sigma[1]==.(sigma[1,1])~", "~mu[2]==.(mu[2])~", "~sigma[2]==.(sigma[2,2])~
+                                                              ", "~sigma[xy]==.(sigma[2,1])))
 
-# Fill a square grid with 0s or 1s according to the probabilityFuncion 
-# The higher is the value probabilityFunction takes in a cetrain point,the
-# higher is the probability that pixel is set to 1.
-faultMap = matrix(data = 0, nrow = length(x1), ncol = length(x2))
-for (i in 1:length(x1)){
-  for (j in 1:length(x2)){
-   if (runif(n = 1, min = 0, max = 1) < (Z[i,j]/max(Z))*maximumFaultProbability){
-    faultMap[i,j] = TRUE 
-   }
-    else{
-      faultMap[i,j] = FALSE
-    } 
-  }
-}
 
-# Creates a circular grid from a square one by inserting the value -1 in
-# every pixel which is not whitin the circle of ray "ray".
-
-for (i in 1:nrow(faultMap)){
-  for (j in 1:ncol(faultMap)){
-    if ((i-ray)^2 + (j-ray)^2 >= ray^2)
-      faultMap[i,j]=NA;
-  }
-}
+faultMap = fillRectangularMap(probabilityFunction = Z, maxFaultProbability = maximumFaultProbability)
+faultMap = bindCircularMap(rectangularMap = faultMap, ray = ray)
 
 # Compute the fault number
-faultNumber = length(faultMap[faultMap==1])
+faultNumber = length(faultMap[(faultMap == TRUE)]) - length(faultMap[is.na(faultMap)])
 
 # Plot the fault map
 par(pty = "s") # Force a square plot
 image2D(
-        x = 1:nrow(faultMap), y = 1:ncol(faultMap), z = faultMap, border = "black", 
-        grid(nx=nrow(faultMap)), ny = ncol(faultMap),
-        colkey = FALSE, NAcol = "white",  col = heat.colors(2)
-        ) # Colkey = FALSE: no color key legend will be added
+  x = 1:nrow(faultMap), y = 1:ncol(faultMap), z = faultMap, border = "black", 
+  grid(nx=nrow(faultMap)), ny = ncol(faultMap),
+  colkey = FALSE, NAcol = "white",  col = heat.colors(2)
+) # Colkey = FALSE: no color key legend will be added
+
+
+########################### FUNCTIONS ######################################
+
+# Creates a circular grid from a square one by inserting the value NA in
+# every pixel which is not whitin the circle of ray "ray".
+bindCircularMap = function(rectangularMap, ray) {
+  for (i in 1:nrow(rectangularMap)){
+    for (j in 1:ncol(rectangularMap)){
+      if ((i-ray)^2 + (j-ray)^2 >= ray^2)
+        rectangularMap[i,j]=NA;
+    }
+  }
+  return(rectangularMap) 
+}
+
+# Fill a square grid with FALSE or TRUE according to the probabilityFuncion 
+# The higher is the value probabilityFunction takes in a cetrain point,the
+# higher is the probability that pixel is set to TRUE.
+fillRectangularMap = function(probabilityFunction, maxFaultProbability){
+  faultMap = matrix(data = 0, nrow = nrow(probabilityFunction), ncol = ncol(probabilityFunction))
+  for (i in 1:nrow(faultMap)){
+    for (j in 1:ncol(faultMap)){
+      if (runif(n = 1, min = 0, max = 1) < (probabilityFunction[i,j]/max(probabilityFunction))*maximumFaultProbability){
+        faultMap[i,j] = TRUE 
+      }
+      else{
+        faultMap[i,j] = FALSE
+      } 
+    }
+  }
+  return(faultMap)
+}
