18af82c744344e760005adb684f688ef1bca00ab
Don Lele <donlelef@gmail.com>
2015-04-02 16:09:54 +0200

    Fixed axes mistake is computing KDE.

--- Matlab/faultMapPlot.m
+++ Matlab/faultMapPlot.m
@@ -17,7 +17,7 @@ x1 = 0:1:2*ray;
 x2 = x1;
 
 mean = [ray,ray];
-normalizedVariance = [1, 0; 0, 1];
+normalizedVariance = [1, 0; 0, 5];
 variance = ray.*normalizedVariance;
 
 [X1,X2] = meshgrid(x1,x2);
@@ -44,14 +44,14 @@ pcolor(faultMap)
 title('Fault map')
 
 % KDE
-% call the routine, which has been saved in the current directory 
-[i, j] = find(faultMap==1);
+% call the routine, which has been saved in the current directory
+[j, i] = find(faultMap==1); % find seachs elements by columns
 [bandwidth,density,X,Y]=kde2d([i,j], 64);
 % plot the data and the density estimate
 figure(3)
 hold on
 contour3(X,Y,density,10); 
-plot(i,j,'r.','MarkerSize',5)
+plot(i,j,'r.','MarkerSize',5);
 title('Extimated density contours')
 figure(4)
 surf(X,Y,density);
@@ -60,17 +60,27 @@ title('Extimated function')
 figure(5)
 subplot(2,2,1)
 surf(X1,X2,Z)
+xlabel('x');
+ylabel('y');
 title('Probability density function')
 subplot(2,2,2)
 pcolor(faultMap)
+xlabel('x');
+ylabel('y');
 title('Fault map')
 subplot(2,2,3)
 hold on
 contour3(X,Y,density,10); 
 plot(i,j,'r.','MarkerSize',5)
+xlim([0, 2*ray]);
+ylim([0, 2*ray]);
+xlabel('x');
+ylabel('y');
 title('Extimated density contours')
 subplot(2,2,4)
 surf(X,Y,density);
 xlim([0, 2*ray]);
 ylim([0, 2*ray]);
+xlabel('x');
+ylabel('y');
 title('Extimated function')
