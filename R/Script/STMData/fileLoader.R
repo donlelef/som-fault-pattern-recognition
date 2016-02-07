6aa22ecdb682fd39802b009de553cb6a01b4c0f2
donlelef <donlelef@gmail.com>
2016-02-07 17:10:33 +0100

    Obsolete files deleted

--- R/Script/STMData/fileLoader.R
+++
@@ -1,4 +0,0 @@
-# This script loads and sava STM data.
-
-library(STMWrapper)
-data = readExcel(xlsToRead = "STMData/LOTTO1_DEF.xlsx", saveOnRDS = TRUE, rdsToWrite = "lotto1.rds")
--- R/Tests/randomTest/LinearOptimizationTest.R
+++
@@ -1,5 +0,0 @@
-x <- 1:10
-y <- x + c(-0.5,0.5)
-
-polyfit <- function(i) x <- AIC(lm(y~poly(x,i)))
-as.integer(optimize(polyfit,interval = c(1,length(x)-1))$minimum)
