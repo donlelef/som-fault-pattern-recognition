2c46a6b517272f55ad6a9fb711a4649faca23978
Emanuele Fabbiani <donlelef@gmail.com>
2016-04-18 00:06:37 +0200

    RootCauseFinderReal completed with bar plot. Resulting data.frame contains now an indication of the number of good and bad wafers. Package project files renamed.

--- Scripts/RootCauseFinderReal.R
+++ Scripts/RootCauseFinderReal.R
@@ -1,9 +1,11 @@
 # Find the equipment wich is most likely to be responsible of 
 # a particular pattern of faults
 library(lubridate)
+library(ggplot2)
+library(ggthemes)
 
 # params
-selectedCluster = 5
+selectedCluster = 4
 threshold = 100
 
 # Import
@@ -52,10 +54,38 @@ for (operation in unique(clusterHistory$OPERATION)) {
       if(badEquipment / (goodEquipment + badEquipment) > badOther / (goodOther + badOther)){
         contingencyMatrix = matrix(c(badEquipment, goodEquipment, badOther, goodOther), nrow = 2, ncol = 2, byrow = FALSE)
         testValue = chisq.test(x = contingencyMatrix, correct = FALSE)
-        res = rbind(res, data.frame(OPERATION = operation, EQUIPMENT = equipment, VALUE = testValue$statistic, stringsAsFactors = FALSE))  
+        res = rbind(res, 
+                    data.frame(OPERATION = operation, 
+                               EQUIPMENT = equipment, 
+                               VALUE = testValue$statistic, 
+                               BAD_EQUIPMENT = badEquipment,
+                               GOOD_EQUIPMENT = goodEquipment,
+                               BAD_OTHER = badOther,
+                               GOOD_OTHER = goodOther,
+                               P_VALUE = testValue$p.value,
+                               stringsAsFactors = FALSE))  
       }
     }
   }
 }
 res = res[order(res$VALUE, decreasing = TRUE), ]
+
+# Plot data
+plotData = unique.data.frame(res[,2:3])[1:10, ]
+plotData$EQUIPMENT = factor(plotData$EQUIPMENT, levels = rev(plotData$EQUIPMENT))
+ggplot(data = plotData, mapping = aes(x = EQUIPMENT, y = VALUE)) +
+  geom_bar(mapping = aes(fill = VALUE), stat = "identity", position = position_dodge(), show.legend = FALSE) +
+  coord_flip(ylim = c(min(plotData$VALUE)*0.9, max(plotData$VALUE)*1.1)) +
+  ggtitle("Top 10 Chi squared values") +
+  theme_classic(base_family = "serif") + 
+  theme(panel.grid.major.x = element_line(colour = "black", size = 0.5, linetype = "dotted"),
+        plot.background = element_rect(fill = "transparent", colour = NA),
+        panel.background = element_rect(fill = "transparent", colour = NA),
+        panel.ontop = TRUE) +
+  scale_fill_gradient(low = "#9999ff", high = "#000066", guide = FALSE)
+  
+# view data frame
+View(res)
+
+
 
